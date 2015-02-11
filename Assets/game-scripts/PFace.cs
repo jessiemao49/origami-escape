e0e5ab5960a255e62378708b30601081e123fd96
Jessie Mao <jessiemao@hntvlan569.1271.wlan.wireless-resnet.upenn.edu>
2015-02-11 02:04:44 -0500

    Ok actually added scripts this time

---
+++ Assets/game-scripts/PFace.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PFace : MonoBehaviour {
+
+	private System.Collections.Generic.List<PVertex> verts;
+	
+	public PFace() {
+		verts = new System.Collections.Generic.List<PVertex> ();
+	}
+
+	public void addVert(PVertex v) {
+		verts.Add (v);
+	}
+
+
+
+}
---
+++ Assets/game-scripts/PaperFold.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperFoldTest : MonoBehaviour {
+
+	private Mesh mesh;
+	private bool faceSelected;
+
+	void Start() {
+//		gameObject.AddComponent("MeshFilter");
+//		gameObject.AddComponent("MeshRenderer");
+		mesh = GetComponent<MeshFilter>().mesh;
+		faceSelected = false;
+	}
+
+	void Update() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100.0f)) {
+
+			// Draw a ray for debug
+			Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+
+			if(Input.GetMouseButtonDown(0)) {
+				if (faceSelected) {
+
+				}
+
+
+				Debug.Log("Mouse Down Hit the following triangle: " + hit.triangleIndex);
+				Vector3[] vertices = mesh.vertices;
+				int[] triangles = mesh.triangles;
+				Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
+				Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
+				Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
+				Transform hitTransform = hit.collider.transform;
+				p0 = hitTransform.TransformPoint(p0);
+				p1 = hitTransform.TransformPoint(p1);
+				p2 = hitTransform.TransformPoint(p2);
+
+				// Outline triangle for debug
+				Debug.DrawLine(p0, p1, Color.green, 2, false);
+				Debug.DrawLine(p1, p2, Color.green, 2, false);
+				Debug.DrawLine(p2, p0, Color.green, 2, false);
+
+				vertices[triangles[hit.triangleIndex * 3 + 0]].y += 2;
+
+				mesh.vertices = vertices;
+				mesh.RecalculateBounds();
+				mesh.RecalculateNormals();
+				mesh.Optimize();
+			}
+		}
+	}
+	
+}
