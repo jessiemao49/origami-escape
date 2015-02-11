528a1ecc32dac024d607a1faf9e60ac4c255a478
Jessie Mao <jessiemao@hntvlan569.1271.wlan.wireless-resnet.upenn.edu>
2015-02-11 01:59:14 -0500

    still trying to add scripts...

---
+++ Assets/game_scripts/PEdge.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PEdge : MonoBehaviour {
+
+	private PVertex p0;
+	private PVertex p1;
+
+	public PEdge (PVertex point0, PVertex point1) {
+	}
+
+}
---
+++ Assets/game_scripts/PVertex.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PVertex : MonoBehaviour {
+
+	private Vector3 pos;
+	private System.Collections.Generic.List<PEdge> neighbors;
+	
+	public PVertex (Vector3 xyz) {
+		pos = xyz;
+		neighbors = new System.Collections.Generic.List<PEdge>();
+	}
+
+	public void move(Vector3 xyz) {
+		pos = xyz;
+	}
+	
+	public void addNeighbor(PEdge n) {
+		neighbors.Add (n);
+	}
+
+	public void removeNeighbor(PEdge n) {
+		neighbors.Remove (n);
+	}
+
+
+}
